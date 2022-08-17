8d2967747da200ef03ee58741c644553cd7845c8
teddy <teddytw1209@gmail.com>
2022-08-17 15:33:12 +0800

    for new data

--- ada_aug/datasets/PTBXL.py
+++ ada_aug/datasets/PTBXL.py
@@ -12,6 +12,24 @@ from .base import BaseDataset
 MAX_LENGTH = 1000
 LABEL_GROUPS = {"all":71, "diagnostic":44, "subdiagnostic":23, "superdiagnostic":5, "form":19, "rhythm":12}
 
+def make_split_indices(test_k,each_lens,sub_tr_ratio):
+    tot_k = len(each_lens)
+    tr_idx,valid_idx,test_idx = np.array([]),None,None
+    start = 0
+    for i in range(tot_k):
+        each_len = each_lens[i]
+        fold_idx = np.arange(each_len) + start
+        if i==test_k:
+            test_idx = fold_idx
+        elif i==(test_k-1+10)%10:
+            valid_idx = fold_idx
+        else:
+            tr_idx = np.concatenate([tr_idx,fold_idx],axis=0).astype(int)
+
+        start += each_len
+    #k, ratio, sub_tr_idx, valid_idx, test_idx
+    return [test_k, sub_tr_ratio, tr_idx, valid_idx, test_idx]
+
 class PTBXL(BaseDataset):
     def __init__(self, dataset_path, labelgroup="diagnostic",multilabel=True,mode='all',denoise=False,
     transfroms=[],augmentations=[],class_augmentations=[],label_transfroms=[],**_kwargs):
@@ -48,12 +66,12 @@ class PTBXL(BaseDataset):
         #file_list = os.listdir(os.path.join(self.dataset_path,self.labelgroup))
         self.input_data = None
         self.label = None
-        if self.denoise:
-            X_from = 'X_%s_denoise.npy'
-        else:
-            X_from = 'X_%s.npy'
         if self.multilabel:
-            y_from = 'y_%s.npy'
+            X_from = 'X_%s_ml.npy'
+        else:
+            X_from = 'X_%s_single.npy'
+        if self.multilabel:
+            y_from = 'y_%s_ml.npy'
         else:
             y_from = 'y_%s_single.npy'
         if mode=='all':
@@ -68,6 +86,27 @@ class PTBXL(BaseDataset):
                 start += each_len
             self.input_data = np.concatenate(datas,axis=0).astype(float)
             self.label = np.concatenate(labels,axis=0).astype(int)
+        elif mode=='foldall':
+            X_from = 'X_fold%d_raw.npy'
+            if self.multilabel:
+                y_from = 'y_fold%d_ml.npy'
+            else:
+                y_from = 'y_fold%d_single.npy'
+            datas,labels = [],[]
+            each_lens = []
+            for f in range(1,11):
+                data = np.load(os.path.join(self.dataset_path,self.labelgroup,X_from%f),allow_pickle=True)
+                label = np.load(os.path.join(self.dataset_path,self.labelgroup,y_from%f),allow_pickle=True)
+                datas.append(data)
+                labels.append(label)
+                each_len = len(label)
+                each_lens.append(each_len)
+            self.input_data = np.concatenate(datas,axis=0).astype(float)
+            self.label = np.concatenate(labels,axis=0).astype(int)
+            self.split_indices = [] #k, ratio, sub_tr_idx, valid_idx, test_idx
+            for k in range(10): #10folds
+                each_split = make_split_indices(k,each_lens,self.sub_tr_ratio)
+                self.split_indices.append(each_split)
         elif mode=='tottrain':
             datas,labels = [0,0],[0,0]
             for i,type in enumerate(['train','test']):
@@ -82,11 +121,12 @@ class PTBXL(BaseDataset):
     def _get_data_fold(self,folds):
         self.input_data = None
         self.label = None
-        X_from = 'X_fold%d_raw.npy'
         if self.multilabel:
             y_from = 'y_fold%d_ml.npy'
+            X_from = 'X_fold%d_ml.npy'
         else:
             y_from = 'y_fold%d_single.npy'
+            X_from = 'X_fold%d_raw.npy'
         datas,labels = [],[]
         for f in folds:
             data = np.load(os.path.join(self.dataset_path,self.labelgroup,X_from%f),allow_pickle=True)

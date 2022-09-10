77c3bd80e68a28683a6bb7bcba09a6de265b490d
teddy <teddytw1209@gmail.com>
2022-09-10 09:02:31 +0800

    Update class_balanced_loss.py

--- ada_aug/class_balanced_loss.py
+++ ada_aug/class_balanced_loss.py
@@ -45,7 +45,7 @@ class FocalLoss(nn.Module):
             input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
             input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1,1)
+        target = target.view(-1,1).long()
         ones = - torch.ones(input.shape).cuda()
         for i in range(input.shape[0]):
             ones[i, target[i,0].item()] = 1 

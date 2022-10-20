545497587a7709eb42995d2113efb09ade85d715
teddy <teddytw1209@gmail.com>
2022-10-20 11:36:12 +0800

    bug fix

--- ada_aug/operation_tseries.py
+++ ada_aug/operation_tseries.py
@@ -1107,12 +1107,12 @@ class KeepAugment(object): #need fix
         self.save_dir = save_dir
         self.selective = None
         if adapt_target not in ['len','seg','way','ch']:
-            print('Keep Auto select: ',target)
             target = adapt_target
+            print('Keep Auto select: ',target)
             if 'cut' in target:
                 self.default_select = 'cut'
             else:
-                self.default_select = 'cut'
+                self.default_select = 'paste'
             if 're' in target:
                 self.reverse = True
             else:

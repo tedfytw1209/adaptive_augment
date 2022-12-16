fix gradient match

ada_aug/gradient_match.py
@@ -71,18 +71,18 @@ def get_loss(enc, x_batch_ecg, seqlen, y_batch, loss_obj,use_mix=False,multilabe
     loss = loss_obj(yhat.squeeze(), y_batch.squeeze())
     return loss
 
-def do_aug(x,seqlen, y, aug,n_class,class_adaptive=False,multilabel=False):
+def do_aug(x,seqlen, y, aug,n_class,class_adaptive=False,multilabel=False,update_w=True):
     policy_y = None
     if class_adaptive: #target to onehot
         if not multilabel:
             policy_y = nn.functional.one_hot(y, num_classes=n_class).cuda().float()
         else:
             policy_y = y.cuda().float()
-    mix_fea, _ = aug(x,seqlen, y=policy_y, mode='explore',update_w=False)
+    mix_fea, _ = aug(x,seqlen, y=policy_y, mode='explore',update_w=update_w)
     return mix_fea
 
 def hyper_step(model, aug, hyper_params, train_loader, optimizer, val_loader, elementary_lr, neum_steps, device, loss_obj,
-        n_class,search_round=4,class_adaptive=False,multilabel=False):
+        n_class,search_round=4,class_adaptive=False,multilabel=False,update_w=True):
     zero_hypergrad(hyper_params)
     num_weights = sum(p.numel() for p in model.parameters())
     d_train_loss_d_w = torch.zeros(num_weights).to(device)
@@ -92,8 +92,8 @@ def hyper_step(model, aug, hyper_params, train_loader, optimizer, val_loader, el
     for batch_idx, (x, seqlen, y) in enumerate(train_loader):
         x = x.to(device).float()
         y = y.to(device)
-        x = do_aug(x,seqlen, y, aug,n_class,class_adaptive,multilabel)
-        train_loss= get_loss(model, x, seqlen, y, loss_obj,use_mix=True,multilabel=multilabel) / search_round
+        fea_x = do_aug(x,seqlen, y, aug,n_class,class_adaptive,multilabel,update_w)
+        train_loss= get_loss(model, fea_x, seqlen, y, loss_obj,use_mix=True,multilabel=multilabel) / search_round
         optimizer.zero_grad()
         d_train_loss_d_w += gather_flat_grad(grad(train_loss, list(model.parameters()), 
                                                   create_graph=True, allow_unused=True))

ada_aug/step_function.py
@@ -1324,13 +1324,16 @@ def search_train_neumann(args, train_queue, search_queue, tr_search_queue, gf_mo
             h_optimizer.zero_grad()
             #input_search_list,seq_len_list,target_search_list,policy_y_list = [],[],[],[]
             #gradient match hyper step
-            elementary_lr = 0.0001 #tmp
-            neum_steps = 5
+            #current train lr
+            for param_group in gf_optimizer.param_groups:
+                elementary_lr = param_group['lr']
+                break
+            neum_steps = 1 #follow taskaug
             use_device = torch.device('cuda')
             hyp_params = list(adaaug.h_model.parameters())
             hyper_grad,diff_loss,sea_loss,input_search_list,seq_len_list,target_search_list,policy_y_list = \
                 hyper_step(gf_model,adaaug,hyp_params,tr_search_queue,gf_optimizer,search_queue,elementary_lr,
-                neum_steps,use_device,sim_criterion,n_class,search_round,class_adaptive,multilabel)
+                neum_steps,use_device,sim_criterion,n_class,search_round,class_adaptive,multilabel,update_w=diff_update_w)
             aug_diff_loss += diff_loss
             aug_search_loss += sea_loss
             hyper_grad_avg += hyper_grad.detach().mean().item()
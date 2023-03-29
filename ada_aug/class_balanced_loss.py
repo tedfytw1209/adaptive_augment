d5268953828b3df539c3ad92eb090317d078e0d3
teddy <teddytw1209@gmail.com>
2023-03-29 18:06:11 +0800

    Update class_balanced_loss.py

--- ada_aug/class_balanced_loss.py
+++ ada_aug/class_balanced_loss.py
@@ -459,6 +459,24 @@ class ClassDistLoss(torch.nn.Module):
             print('similar loss: ',classdist_loss) #!tmp
         return classdist_loss
 
+#class KD loss ref, not using
+class CriterionKD(nn.Module):
+    '''
+    knowledge distillation loss
+    '''
+    def __init__(self, temperature=1):
+        super(CriterionKD, self).__init__()
+        self.temperature = temperature
+
+    def forward(self, pred, soft):
+        B, C, h, w = soft.size()
+        scale_pred = pred.permute(0,2,3,1).contiguous().view(-1,C)
+        scale_soft = soft.permute(0,2,3,1).contiguous().view(-1,C)
+        p_s = F.log_softmax(scale_pred / self.temperature, dim=1)
+        p_t = F.softmax(scale_soft / self.temperature, dim=1)
+        loss = F.kl_div(p_s, p_t, reduction='batchmean') * (self.temperature**2)
+        return loss
+
 #train class weight loss
 def compute_class_weights_dict(train_y, n_class): #!!!need change
     class_weights_dict = None
